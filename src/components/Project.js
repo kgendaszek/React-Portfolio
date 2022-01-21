ccceedd7444353d67beeffd6df194c604870b65d
KatieG <katieshake27@gmail.com>
2022-01-21 15:29:49 -0500

    fixed images

--- src/components/Project.js
+++ src/components/Project.js
@@ -5,17 +5,16 @@ import React from "react";
 function Project({ image, title, description, gitHub, deployed }) {
   return (
     <div className="card col-md-4">
-      {/* <img className="card-img-top"src={require('../components/images/' + {image})} alt="Card cap"/> */}
-      <img className="card-img" src={image} alt="Card cap" />
+      <img className="card-img projectimg d-flex" src={image} alt="Card cap" />
       <div className="card-body">
-        <h5 className="card-title">Title: {title}</h5>
-        <p className="card-text">Description: {description}</p>
+        <h5 className="card-title">{title}</h5>
+        <p className="card-text">{description}</p>
         <div className="card-footer">
         <div className="d-flex justify-content-evenly">
-        <a href={gitHub} className="btn btn-primary project-link">
+        <a href={gitHub} target="_blank" className="btn btn-primary project-link">
           GitHub Repo
         </a>
-        <a href={deployed} className="btn btn-primary project-link">
+        <a href={deployed} target="_blank" className="btn btn-primary project-link">
           Deployed Site
         </a>
         </div>
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,47 +1,55 @@
-import { url } from "inspector";
+// import { url } from "inspector";
 import React from "react";
 import Project from "../Project";
+import refactor from "../../images/Final-product.jpeg"
+import pwgen from "../../images/pwgen.png"
+import coco from "../../images/coco-quiz-final.png"
+import ogport from "../../images/portfolioss.png"
+import barker from "../../images/bark.png"
+import hypo from "../../images/hypo-login.png"
+
+
 
 
 export default function Projects(props) {
   const allProjects = [
     {
-      image: "../../../public/images/Final-product.jpeg",
+      image: refactor,
       title: "HTML Refactoring",
       description: "This assignment was to refactor this page to HTML 5 semantics.",
       gitHub: "https://github.com/kgendaszek/Refactor-1",
       deployed: "https://kgendaszek.github.io/Refactor-1/",
     },
     {
-      image: "../../../src/components/images/pwgen.png",
+      image: pwgen,
       title: "Password Generator",
       description: "This was my first project using JavaScript. As such, it proved an exciting challenge and a great learning experience.",
       gitHub: "https://github.com/kgendaszek/password-generator",
       deployed: "https://kgendaszek.github.io/password-generator/",
     },
     {
-      image: "../src/components/images/coco-quiz-final.png",
+      image: coco,
       title: "Quiz made with JavaScript",
       description: "The code quiz was a challenge! It was made with JavaScript, and it was my first experience working with local storage.",
       gitHub: "https://github.com/kgendaszek/quiz",
       deployed: "https://kgendaszek.github.io/quiz/",
     },
     {
-      image: "../../src/components/images/portfolioss.png",
-      title: "Original Portfolio made with HTML and CSS",
+      image: ogport,
+      title: "Original Portfolio",
       description: "This was my original portfolio made with just HTML and CSS. I've included it just to show how far I've come!",
       gitHub: "https://github.com/kgendaszek/Portfolio",
       deployed: "https://kgendaszek.github.io/Portfolio/",
     },
     {
-      image: "src/components/images/final-with-results.png",
+      image: barker,
       title: "Barker",
       description: "Barker is an application I helped to create.It uses different API's to help the user decide what dog they should get & where the closest PetSmart is.",
       gitHub: "https://github.com/SteveKoutsodontis/Barker",
       deployed: "https://stevekoutsodontis.github.io/Barker/",
     },
     {
-      image: "src/components/images/hypo-login.png",
+      image: hypo,
       title: "Hypochondriacs",
       description: "Hypochondriacs is an auction application that allows users to place bids on popular pandemic products. Each time someone clicks on the bid button it records the amount of bids.",
       gitHub: "https://github.com/negronmarc/Auction-for-Pandemic-Essentials",
