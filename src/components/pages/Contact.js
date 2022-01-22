4d828a172de4c41becb44d4a2133fa975a649654
KatieG <katieshake27@gmail.com>
2022-01-21 20:33:50 -0500

    added target blank to contact links

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -8,22 +8,22 @@ export default function Contact() {
       <h1>Contact Information: </h1>
       <h5>
         Call/text me at:
-        <a href="tel:856-693-8190">856-693-8190</a>
+        <a target="_blank" href="tel:856-693-8190">856-693-8190</a>
       </h5>
 
       <h5>
         Email me at:{" "}
-        <a href="mailto:katieshake27@gmail.com">katieshake27@gmail.com</a>
+        <a target="_blank" href="mailto:katieshake27@gmail.com">katieshake27@gmail.com</a>
       </h5>
       <h2>Professional Sites:</h2>
       <h3>
-        <a href="https://github.com/kgendaszek">GitHub</a>
+        <a target="_blank" href="https://github.com/kgendaszek">GitHub</a>
       </h3>
       <h3>
-        <a href="https://www.linkedin.com/in/katherine-gendaszek">LinkedIn</a>
+        <a target="_blank" href="https://www.linkedin.com/in/katherine-gendaszek">LinkedIn</a>
       </h3>
       <h3>
-        <a href="https://docs.google.com/document/d/1VpI-rN4TVcuIP6O5eL94WStu2tVJB-5NctUQlEn0MZM/edit?usp=sharing">
+        <a target="_blank" href="https://docs.google.com/document/d/1VpI-rN4TVcuIP6O5eL94WStu2tVJB-5NctUQlEn0MZM/edit?usp=sharing">
           Resume
         </a>
       </h3>
